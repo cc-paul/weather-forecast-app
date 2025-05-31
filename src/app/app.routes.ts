ca1db46d0fde7bac716c67f6b6cb59c77a2d0527
paulpagente <paulfelco0@gmail.com>
2025-05-31 14:31:56 +0800

    [5/31/2025] Created Base Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./landing/landing.component').then(m => m.LandingComponent)
+    }
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
