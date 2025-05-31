5967b916a4d2d32d7cb49cdf2d126e9071426e78
paulpagente <paulfelco0@gmail.com>
2025-05-31 17:58:09 +0800

    [05/31/2025] Added design on the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  userFullName: string = "";
+  userGitProfileLink: string = "";
 
+  ngOnInit(): void {
+    this.userFullName = 'John Doe';
+    this.userGitProfileLink = 'github.com/johndoe';
+  }
 }
---
+++ src/app/shared/constant/messages.ts
@@ -0,0 +1,3 @@
+export const Messages = {
+    welcomeMessage: "Welcome to the weather forecast web application. Please login with your\nGithub user to use the application and view the weather in your city"
+}
